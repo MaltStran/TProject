c0b452e50f7f0ef6124758ff1d4c351c406f50d9
MaltStran <niknik2430@mail.ru>
2023-05-18 20:16:03 +0500

    Controller fix

--- bot/src/main/java/ru/tinkoff/edu/java/bot/controller/GlobalExceptionHandler.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/controller/GlobalExceptionHandler.java
@@ -14,21 +14,37 @@ import ru.tinkoff.edu.java.bot.model.controller.ApiErrorResponse;
         basePackages = "ru.tinkoff.edu.java.bot.controller"
 )
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    private static final String TG_API_ERROR_CODE = "tg-api";
-    private static final String SERVER_ERROR_CODE = "server";
+    private static final String TG_API_ERROR_CODE = "Telegram-API";
+    private static final String SERVER_ERROR_CODE = "Server";
 
     private static final String TG_API_ERROR_DESCRIPTION = "Error happened while sending updates";
     private static final String SERVER_ERROR_DESCRIPTION = "Internal error";
 
-    @ExceptionHandler(value = {TelegramApiException.class})
-    private ResponseEntity<ApiErrorResponse> handleTelegramApiException(TelegramApiException ex) {
-        return buildError(
-                ex,
-                HttpStatus.BAD_REQUEST,
-                TG_API_ERROR_CODE,
-                TG_API_ERROR_DESCRIPTION
+
+
+
+
+    private ResponseEntity<ApiErrorResponse> buildError(
+        Exception exception,
+        HttpStatus httpStatus,
+        String code,
+        String description
+    ) {
+        return new ResponseEntity<>(
+            new ApiErrorResponse(
+                description,
+                code,
+                exception.toString(),
+                exception.getMessage(),
+                Arrays.stream(exception.getStackTrace())
+                    .map(Objects::toString)
+                    .toList()
+            ),
+            httpStatus
         );
     }
+
+
 
     @ExceptionHandler
     protected ResponseEntity<ApiErrorResponse> handleOtherErrors(Exception ex) {
@@ -40,23 +56,20 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
     }
 
-    private ResponseEntity<ApiErrorResponse> buildError(
-            Exception exception,
-            HttpStatus httpStatus,
-            String code,
-            String description
-    ) {
-        return new ResponseEntity<>(
-                new ApiErrorResponse(
-                        description,
-                        code,
-                        exception.toString(),
-                        exception.getMessage(),
-                        Arrays.stream(exception.getStackTrace())
-                                .map(Objects::toString)
-                                .toList()
-                ),
-                httpStatus
+
+
+    @ExceptionHandler(value = {TelegramApiException.class})
+    private ResponseEntity<ApiErrorResponse> handleTelegramApiException(TelegramApiException ex) {
+        return buildError(
+            ex,
+            HttpStatus.BAD_REQUEST,
+            TG_API_ERROR_CODE,
+            TG_API_ERROR_DESCRIPTION
         );
     }
+
+
+
+
+
 }
