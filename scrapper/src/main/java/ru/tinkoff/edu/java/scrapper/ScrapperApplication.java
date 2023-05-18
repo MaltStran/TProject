e53f723bf9685824031efb0282a4d2b932b2746a
MaltStran <niknik2430@mail.ru>
2023-05-18 21:07:22 +0500

    Grand Finale fix, its all breaked, retreat

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -1,16 +1,18 @@
 package ru.tinkoff.edu.java.scrapper;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
 
-@Slf4j
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
-        ApplicationConfig conf = ctx.getBean(ApplicationConfig.class);
-        log.info(conf.toString());
+        ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
+        System.out.println(config);
     }
 }
